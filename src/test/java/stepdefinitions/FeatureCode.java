ffbf49ae61432ae605bd5ab23b2385cc2f3a0418
dipak922 <dipaksahane450@gmail.com>
2025-11-03 17:05:02 +0530

    feature branch first commit

---
+++ src/test/java/stepdefinitions/FeatureCode.java
@@ -0,0 +1,18 @@
+package stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FeatureCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://github.com/dipak922/eclipse-Project/blob/master/src/test/java/stepdefinitions/Monday.java");
+	driver.close();
+	
+	
+	
+	}
+
+}
