e15430738809f128fc95aaa2b061c7c9fe11f4fe
dipak922 <dipaksahane450@gmail.com>
2025-11-03 16:40:42 +0530

    third commit

--- src/test/java/stepdefinitions/Monday.java
+++ src/test/java/stepdefinitions/Monday.java
@@ -8,6 +8,7 @@ public class Monday {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 WebDriver driver =new ChromeDriver();
+driver.get("https://github.com/dipak922/eclipse-Project/blob/master/src/test/java/stepdefinitions/Monday.java");
 	}
 
 }
